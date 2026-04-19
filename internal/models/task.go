e2c20dc37f12fc790531d600aa27636c0416087e
Potehin Vladimir <251884541+Potehin-Vladimir@users.noreply.github.com>
2026-04-19 14:13:37 +0400

    feat: add Task constructor

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,6 +1,15 @@
 package models
 
-import "time"
+import (
+	"sync/atomic"
+	"time"
+)
+
+var lastTaskId atomic.Int64
+
+func getNextID() int64 {
+	return lastTaskId.Add(1)
+}
 
 type Task struct {
 	ID          int64
@@ -12,4 +21,41 @@ type Task struct {
 	CreatedAt   time.Time
 	DueDate     *time.Time
 	IsCompleted bool
+}
+
+type TaskOption func(*Task)
+
+func WithPriority(p int) TaskOption {
+	return func(t *Task) {
+		t.Priority = p
+	}
+}
+
+func WithDescription(d string) TaskOption {
+	return func(t *Task) {
+		t.Description = d
+	}
+}
+
+func WithDueDate(d time.Time) TaskOption {
+	return func(t *Task) {
+		t.DueDate = &d
+	}
+}
+
+func NewTask(title string, userID int64, opts ...TaskOption) *Task {
+	task := &Task{
+		ID:          getNextID(),
+		UserID:      userID,
+		Title:       title,
+		Priority:    1,
+		CreatedAt:   time.Now(),
+		IsCompleted: false,
+	}
+
+	for _, opt := range opts {
+		opt(task)
+	}
+
+	return task
 }
