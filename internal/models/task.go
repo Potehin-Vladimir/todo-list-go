b119a36a957ed9f1ba5df2aa99092eb92257db11
Potehin Vladimir <251884541+Potehin-Vladimir@users.noreply.github.com>
2026-04-19 13:18:02 +0400

    feat: add struct task

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,1 +1,15 @@
 package models
+
+import "time"
+
+type Task struct {
+	ID          int
+	Title       string
+	Description string
+	Priority    int
+
+	CreatedAt   time.Time
+	ExpiredAt   time.Time
+	DueDate     time.Time
+	IsCompleted bool
+}
